5855d728fc5d07b7bfa2e1d8d112478d4fcd8999
Robert Hambrock <roberthambrock@gmail.com>
2024-08-20 01:01:49 +0200

    some groundwork for testing

--- packages/foundry/test/Hypertents.t.sol
+++ packages/foundry/test/Hypertents.t.sol
@@ -2,7 +2,50 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "src/Hypertents.sol";
+
+address constant FAUCET_A = address(0x01);
+address constant FAUCET_B = address(0x02);
+
+address constant ALICE = address(0x03);
+
+
+contract TestTokenFaucet {
+    ITokenFaucet public tokenFaucetContract;
+
+    constructor(address _tokenFaucetAddress) {
+        tokenFaucetContract = ITokenFaucet(_tokenFaucetAddress);
+    }
+
+    function triggerDrip(address recipient) external {
+        tokenFaucetContract.drip(recipient);
+    }
+}
 
 contract BaseTest is Test {
-    function testHypertents() public {}
+    function testInstantiateSameChain() public {
+        // just any from https://github.com/polytope-labs/ismp-solidity/blob/e67af4a359b3a53fa8132f7223c89ba680a24439/interfaces/IIsmpModule.sol#L84-L112
+        vm.chainId(11155111);
+
+        TestTokenFaucet faucetA = new TestTokenFaucet(FAUCET_A);
+        TestTokenFaucet faucetB = new TestTokenFaucet(FAUCET_B);
+        // emit log_address(address(faucetA));
+        // emit log_address(FAUCET_A);
+
+        // instantiate Hypertents with default address
+        Hypertents tentA = new Hypertents(address(faucetA));
+        // Hypertents tentB = new Hypertents(address(faucetB));
+        // create a CrossChainOrder
+        // CrossChainOrder memory order = CrossChainOrder({
+        //     settlementContract: address(FAUCET_B),
+        //     swapper: address(ALICE),
+        //     nonce: 0,
+        //     originChainId: 0,
+        //     initiateDeadline: 0,
+        //     fillDeadline: 0,
+        //     orderData: new bytes(0)
+        // });
+
+        // tentA.initiate(order, new bytes(0), new bytes(0));
+    }
 }
